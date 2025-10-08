9e99aae73d81fdd25074ec7ddaa3f60df14cc50e
haritha <harithawikramasinha2003@gmail.com>
2025-10-08 22:56:14 +0530

    set up project

---
+++ src/main/java/com/example/ecommerceplatformserverside/EcommercePlatformServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceplatformserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePlatformServerSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercePlatformServerSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommerceplatformserverside/EcommercePlatformServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceplatformserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePlatformServerSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
